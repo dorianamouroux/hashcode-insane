2fe27f9d2a271c2ef728387c481818a642050d5f
Jean-Baptiste Grégoire <j.baptiste159@gmail.com>
2016-02-11 20:22:45 +0100

    first step: create main and show how import work

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import insane.Core
+# or
+from insane.Core import func
+
+insane.Core.func()
+# or
+func()
+
+def main():
+    # write your main here
+
+if __name__ == "__main__":
+    main()
