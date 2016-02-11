63c928a4f8569ee37cc785d7639b880676ab1927
Dorian Amouroux <dor.amouroux@gmail.com>
2016-02-11 21:21:39 +0100

    add order item

--- insane/Core.py
+++ insane/Core.py
@@ -2,6 +2,7 @@
 
 from insane.WareHouse import WareHouse
 from insane.Product import Product
+from insane.Order import Order
 
 class Core:
     
@@ -13,6 +14,7 @@ class Core:
         self.maxPayload = maxPayload
         self.WHList = []
         self.productType = []
+        self.orders = []
         
     def addWareHouse(self, posX, posY):
         newWH = WareHouse(posX, posY, len(self.WHList), len(self.productType))
@@ -26,6 +28,22 @@ class Core:
         newProduct = Product(len(self.productType), weight)
         self.productType.append(newProduct)
         return newProduct.getId()
+    
+    def getProductType(self, pos):
+        return self.productType[pos]
 
     def addProductWareHouse(self, idWareHouse, idProduct, quantity):
         self.getWareHouse(idWareHouse).addProduct(idProduct, quantity)
+
+        
+    def addOrder(self, posX, posY):
+        newOrder = Order(posX, posY, len(self.orders), len(self.productType))
+        self.orders.append(newOrder)
+        return newOrder.getId()
+    
+    def getOrder(self, pos):
+        return self.orders[pos]
+    
+    def addItemOrder(self, idOrder, idItem, quantity):
+        product = self.getProductType(idItem)
+        self.getOrder(idOrder).addItem(product, quantity)
---
+++ insane/Order.py
@@ -0,0 +1,20 @@
+# insane team
+
+class Order:
+    
+    def __init__(self, posX, posY, id, nbItem):
+        self.x = posX
+        self.y = posY
+        self.id = id
+        self.weight = 0
+        self.items = [0] * nbItem
+    
+    def addItem(self, product, quantity):
+        self.items[product.getId()] = quantity
+        self.weight += (quantity * product.getWeight())
+    
+    def getId(self):
+        return self.id
+    
+    def getWeight():
+        return self.weight
